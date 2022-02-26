5584e1e162805139cb395006f8f27edf6d52b8c4
Néo Almeida <neo.almeida.etu@univ-lille.fr>
2022-02-26 01:01:06 +0100

    Save system is working atm (Missing OpenAs) | Added much comments | Refractoring

--- Notepad/MainWindow.xaml.cs
+++ Notepad/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Windows.Controls;
 using System.IO;
 using Microsoft.Win32;
 using System.Windows.Input;
+using System.ComponentModel;
 
 namespace Notepad
 {
@@ -12,6 +13,10 @@ namespace Notepad
     /// </summary>
     public partial class MainWindow
     {
+        private bool _changed = false;
+        private string _appname = "Bloc-Notes";
+        private string _filepath  = "";
+        private string _filename = "Sans titre";
 
         public TextBox textbox { get; }
         private Research searchWindow;
@@ -21,146 +26,336 @@ namespace Notepad
             InitializeComponent();
             this.textbox = (TextBox)this.FindName("actualFile");
             this.searchWindow = new Research(this);
+            this.ResetTitle();
         }
 
+
+        /* EVENT HANDELING */
+        
+        /// <summary>
+        ///     Event called on windows close
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnClosing(object sender, CancelEventArgs e)
+        {
+            if (actualFile.Text.Length != 0 && _changed)
+            {
+                e.Cancel = true;
+                AskForSave(() =>
+                {
+                    e.Cancel = false;
+                });
+            }
+        }
+        
+        /// <summary>
+        ///     Event called on text input (Also changing title name)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Text_Changed(object sender, TextChangedEventArgs? e)
+        {
+            if(!_changed)
+            {
+                this.Title += "*";
+                _changed = true;
+            }
+        }
+
+        /* KEYBIND HANDELING */
+
+        /// <summary>
+        ///     Ctrl + N
+        ///     Execute OpenNewFile() function
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         public void Keybind_new(Object sender, ExecutedRoutedEventArgs e)
         {
-            this.Click_newFile(sender, null);
+            this.OpenNewFile();
         }
 
+        /// <summary>
+        ///     F5
+        ///     Execute OpenNewFile() function
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         public void Keybind_date(Object sender, ExecutedRoutedEventArgs e)
         {
             this.AddDate();
         }
 
+        /// <summary>
+        ///     Ctrl + S
+        ///     Execute SaveFile() function
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void Keybind_save(Object sender, ExecutedRoutedEventArgs e)
+        {
+            this.SaveFile();
+        }
+
+        /// <summary>
+        ///     Ctrl + L
+        ///     Execute Close() function
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         public void Keybind_exit(Object sender, ExecutedRoutedEventArgs e)
         {
-            this.Click_Exit(sender, null);
+            this.Close();
         }
+
+        /// <summary>
+        ///     Ctrl + F
+        ///     Execute OpenSearchWindow() function
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         public void Keybind_search(Object sender, ExecutedRoutedEventArgs e)
         {
-            this.Click_searchWord(sender, null);
+            this.OpenSearchWindow();
         }
-        
+
+
+        /* CLICK HANDELING */
+
+        /// <summary>
+        ///     Execute the AddDate() function on trigger
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Click_Date(object sender, RoutedEventArgs? e)
         {
             this.AddDate();
         }
 
-        private void Click_selectAll(object sender, RoutedEventArgs? e)
+        /// <summary>
+        ///     Execute the SaveFile() function on trigger
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Click_Save(object sender, RoutedEventArgs? e)
+        {
+            this.SaveFile();
+        }
+
+        /// <summary>
+        ///     Execute the SelectAll() function on trigger
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Click_SelectAll(object sender, RoutedEventArgs? e)
         {
             this.SelectAll();
         }
 
-        private void Click_searchWord(object sender, RoutedEventArgs? e)
+        /// <summary>
+        ///     Shows off the search window 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Click_SearchWord(object sender, RoutedEventArgs? e)
+        {
+            this.OpenSearchWindow();
+        }
+
+        /// <summary>
+        ///    Execute .Close function, which one, trigger CloseEvent
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Click_Exit(object sender, RoutedEventArgs? e)
+        {
+            this.Close();
+        }
+
+        /// <summary>
+        ///     Execute OpenNewFile() function
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Click_NewFile(object sender, RoutedEventArgs? e)
+        {
+            this.OpenNewFile();
+        }
+
+        /* CORE FUNCTIONS */
+
+        private void OpenSearchWindow()
         {
             this.searchWindow.Show();
         }
 
-        private void Click_Exit(object sender, RoutedEventArgs? e)
+        /// <summary>
+        ///     Asking to the user if he want to save his current file, if save is cancelled nothing gonna happen, BUT, If save is failed/successfull,
+        ///     the method in param will be executed
+        /// </summary>
+        /// <param name="methodName">Method to execute</param>
+        private void AskForSave(Action methodName)
         {
+            var result = MessageBox.Show("Voulez-vous enregistrer les modifications de " + _filename, "Enregistrer", MessageBoxButton.YesNoCancel, MessageBoxImage.Question);
 
-            var result = MessageBox.Show("TU VEUX SAUVEGARDER???",
-                        "Save file", MessageBoxButton.YesNoCancel, MessageBoxImage.Question);
-            
-            // File isnt empty, ask to 
-            if (this.textbox.Text.Length != 0)
+            switch (result)
             {
-                switch (result)
-                {
-                    case MessageBoxResult.Yes:
-                        if (saveFile(this.textbox.Text))
-                        {
-                            Application.Current.Shutdown();
-                        }
-                        break;
-                    case MessageBoxResult.No:
-                        Application.Current.Shutdown();
-                        break;
-                    case MessageBoxResult.Cancel:
-                        break;
-                }
-            }
-            
-        }
-        private void Click_newFile(object sender, RoutedEventArgs? e)
-        {
-            
-
-            if (this.textbox == null)
-            {
-                MessageBox.Show("ActualFile equals null");
-            }
-            else
-            {
-
-                // File isnt empty, ask to save
-                if(actualFile.Text.Length != 0)
-                {
-                    var result = MessageBox.Show("TU VEUX SAUVEGARDER???",
-                        "Save file", MessageBoxButton.YesNoCancel, MessageBoxImage.Question);
-
-                    switch (result)
+                case MessageBoxResult.Yes:
+                    SaveStatement save = SaveFile();
+                    if (save == SaveStatement.Saved)
                     {
-                        case MessageBoxResult.Yes:
-                            if(saveFile(this.textbox.Text))
-                            {
-                                this.ClearText();
-                            }
-                            break;
-                        case MessageBoxResult.No:
-                            this.ClearText();
-                            break;
-                        case MessageBoxResult.Cancel:
-                            break;
+                        methodName();
                     }
-                } 
-                else
-                {
-                    this.ClearText();
-                }
+                    break;
+                case MessageBoxResult.No:
+                    methodName();
+                    break;
+                case MessageBoxResult.Cancel:
+                    break;
             }
         }
 
-        private bool saveFile(String text)
+        /// <summary>
+        ///     Open a save file dialog, and permit to user to save his actual file.
+        ///     If the user, already choose a path, and the program had the file in memory, the functions wont call the save file dialog, and will directly
+        ///     save file inside the saved path
+        /// </summary>
+        /// <returns></returns>
+        private SaveStatement SaveFile()
         {
-
-            bool textSaved = false;
+            SaveStatement save = SaveStatement.NotSaved;
             Stream myStream;
 
             SaveFileDialog dialog = new SaveFileDialog()
             {
                 Filter = "Text Files(*.txt)|*.txt|All(*.*)|*"
             };
-            
-            if (dialog.ShowDialog() == true)
-            {
-                if ((myStream = dialog.OpenFile()) != null)
-                { 
-                    using (StreamWriter sw = new StreamWriter(myStream, System.Text.Encoding.UTF8))
-                    {
-                        sw.WriteLine(text);
-                    }
 
-                    myStream.Close();
-                    textSaved = true;
+            if(_filepath.Equals(""))
+            {
+                if (dialog.ShowDialog() == true)
+                {
+                    if ((myStream = dialog.OpenFile()) != null)
+                    {
+                        FileStream fs = (FileStream)myStream;
+                        using (StreamWriter sw = new StreamWriter(fs, System.Text.Encoding.UTF8))
+                        {
+                            sw.WriteLine(this.textbox.Text);
+                        }
+                        fs.Close();
+                        myStream.Close();
+                        save = SaveStatement.Saved;
+                        this.UpdateTitle(fs);
+                        this.ResetTitle();
+                        _changed = false;
+                    }
+                    else
+                    {
+                        save = SaveStatement.Cancel;
+                    }
                 }
+            } else
+            {
+                FileStream fs = File.Open(_filepath, FileMode.Open);
+                using (StreamWriter sw = new StreamWriter(fs, System.Text.Encoding.UTF8))
+                {
+                    sw.WriteLine(this.textbox.Text);
+                }
+                fs.Close();
+                this.UpdateTitle(fs);
+                this.ResetTitle();
+                _changed = false;
             }
-            return textSaved;
+            return save;
         }
 
+        private void OpenNewFile()
+        {
+            if (this.textbox == null)
+            {
+                MessageBox.Show("ActualFile equals null");
+            }
+            else
+            {
+                if (actualFile.Text.Length != 0 && _changed)
+                {
+                    AskForSave(() =>
+                    {
+                        this.ClearText();
+                        this._filepath = "";
+                        this._filename = "Sans titre";
+                        this.ResetTitle();
+                        _changed = false;
+                    });
+                }
+            }
+        }
+
+        /// <summary>
+        ///     Update title name after a save
+        /// </summary>
+        /// <param name="fs"></param>
+        void UpdateTitle(FileStream fs)
+        {
+            _filepath = fs.Name.ToString();
+            string[] splitedVersion = _filepath.Split("\\");
+
+            _filename = splitedVersion[splitedVersion.Length - 1];
+        }
+
+        /// <summary>
+        ///     Insert today's date at current cursor position
+        /// </summary>
         private void AddDate()
         {
             DateTime date = DateTime.Now;
             this.InsertText(date.ToString());
         }
 
+        /// <summary>
+        ///     Insert text at the current cursor position, carret is handeled to be set at the end of the inserted text
+        /// </summary>
+        /// <param name="text">Text to insert</param>
         private void InsertText(String text)
         {
             int caretPos = this.textbox.CaretIndex;
             this.textbox.Text = this.textbox.Text.Insert(this.textbox.CaretIndex, text);
             this.textbox.CaretIndex = caretPos + text.Length;
         }
+
+        /// <summary>
+        ///     Select whole text
+        /// </summary>
+        private void SelectAll()
+        {
+            this.textbox.SelectAll();
+        }
+
+        /// <summary>
+        ///     Clear textbox's text
+        /// </summary>
+        private void ClearText()
+        {
+            if (this.textbox != null)
+            {
+                this.textbox.Clear();
+            }
+        }
+
+        /// <summary>
+        ///     Set title to correspond to the file
+        ///     For exemple :
+        ///     If you open a file called "Foo.md", Windows's Title will be : {AppName} - Foo.md
+        /// </summary>
+        private void ResetTitle()
+        {
+            this.Title = _appname + " - " + _filename;
+        }
+
+
+
+        /* UNUSED FUNCTIONS */
 
         private void replace()
         {
@@ -186,19 +381,5 @@ namespace Notepad
             this.textbox.Paste();
         }
 
-        private void SelectAll()
-        {
-            this.textbox.SelectAll();
-        }
-
-        private void ClearText()
-        {
-            if (this.textbox != null)
-            {
-                this.textbox.Clear();
-            }
-        }
-
-        
     }
 }
